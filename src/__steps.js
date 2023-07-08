0795e045e10c99b8bfc929e4f3b6f9f164e1d959
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-07-08 22:05:41 +0600

    Router and layout created

--- src/__steps.js
+++ src/__steps.js
@@ -1,9 +1,22 @@
 /*
+BASIC PROJECT SETUP
+----------------------------------
 1. create a new react project
 2. create a firebase app
 3. get firebase configuration
 4. install firebase and react router dom
 5. create firebase configuration.
+6. made sure app exported from firebase.config.js
 
+---------------
+CSS framework
+---------------
+1. installed react-bootstrap
+2. connected css to the index.js file
+
+
+ROUTER SETUP:
+1. Created a route component
+2. use ReactrouterProvider into App js and give the router value;
 
 */
